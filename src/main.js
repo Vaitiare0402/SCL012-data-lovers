e8953999281e2b25d4fb0d4a6298695e0f27bf9b
fer <fernanda.1991.alejandra@gmail.com>
2019-12-06 11:06:38 -0300

    inicio de codigo

--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
-/**
- * import POKEMON from './data/pokemon/pokemon.js'
- * import LoL from './data/lol/lol.js'
- * import POTTER from './data/potter/potter.js'
- */
+ document.getElementById("btn").addEventListener("click", () => {
+     
+
+        pag1.style.display = "none";
+        root.style.display = "block";
+        const text = document.getElementById("nom").value;
+        const text1 =document.getElementById("nom1").value;
+        console.log(text);
+    
+    })
+     
+
+
+
+ 
+ import POTTER from './data/potter/potter.js'
+ 
 
 import { example } from './data.js';
 
 console.log(example);
 
-/*
- * console.log(POKEMON);
- * console.log(LoL);
- * console.log(POTTER)
-*/
+
+console.log(POTTER)
+
