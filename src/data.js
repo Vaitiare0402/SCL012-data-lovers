643fa416c842d1770821b54f87b6133c9b4f1d42
fer <fernanda.1991.alejandra@gmail.com>
2019-12-06 14:46:49 -0300

    el boton muestra la data

--- src/data.js
+++ src/data.js
@@ -1,7 +1,26 @@
+
+import Harry from './data/potter/potter.js';
+
+export const harryp = (type = undefined) => {
+  if(!type){
+    return Harry;
+  }
+  //return Harry.filter(p => p.type.map(t => t.toLowerCase()).indexOf(type.toLowerCase()) >= 0);
+ 
+//};
+
+
+
+
+
+
+
+
+
 /* Manejo de data */
 
 // esta es una función de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+
+  //return 'example';
+//};
--- src/main.js
+++ src/main.js
@@ -1,25 +1,40 @@
- document.getElementById("btn").addEventListener("click", () => {
+ 
+    import POTTER from './data/potter/potter.js'
+    
+    //btn de la pag principal//
+     document.getElementById("btn").addEventListener("click", () => {
      
-
         pag1.style.display = "none";
         root.style.display = "block";
         const text = document.getElementById("nom").value;
-        const text1 =document.getElementById("nom1").value;
+        const text1 =document.getElementById("nom1");
         console.log(text);
-    
+        text1.innerHTML = text.value;
     })
-     
+    //btn filtrado//
+    let harryF = [];
+    const data = document.getElementById("btn2").addEventListener("click", () =>{
+    //con el for recorre toda la data//
+    for(let i = 0; i < POTTER.length; i++){
+        const btn = POTTER[i];
+        console.log(POTTER[i]);
+        }
+        
+    })
+//hacer console.log con todas las caracteristicas//
 
+ //console.log(POTTER)
+ //console.log(POTTER[0].name);
+ //console.log(POTTER[1].species);
+ //console.log(POTTER[2].gender);
+ //console.log(POTTER[3].house);
+ //console.log(POTTER[4].dateOfBirth);
+ //console.log(POTTER[5].yearOfBirth);
+ //console.log(POTTER[6].ancestry);
+ //console.log(POTTER[7].eyeColour);
+ //console.log(POTTER[8].hairColour);
 
-
- 
- import POTTER from './data/potter/potter.js'
- 
-
-import { example } from './data.js';
-
-console.log(example);
-
-
-console.log(POTTER)
+    
+    
+    
 
