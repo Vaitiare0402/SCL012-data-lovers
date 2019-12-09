b0a63f0301fc0586c2617874d9939a4421de2ce4
Vaitiare0402 <ihopetimedoesntchangehim@gmail.com>
2019-12-09 13:55:48 -0300

    Botones en las casas

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
+document.getElementById("btn").addEventListener("click", () => {
+     
+
+    pag1.style.display = "none";
+    root.style.display = "block";
+    const text = document.getElementById("nom").value;
+    const text1 =document.getElementById("nom1").value;
+    console.log(text);
+    
+
+})
 /**
  * import POKEMON from './data/pokemon/pokemon.js'
  * import LoL from './data/lol/lol.js'
