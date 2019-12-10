6ba46428a0cf7280f6a3b3c8731a1363c6c65590
fer <fernanda.1991.alejandra@gmail.com>
2019-12-10 14:49:03 -0300

    el btn2 filtra las casas de Gryffindor

--- src/data.js
+++ src/data.js
@@ -1,26 +1,4 @@
-
-import Harry from './data/potter/potter.js';
-
-export const harryp = (type = undefined) => {
-  if(!type){
-    return Harry;
-  }
-  //return Harry.filter(p => p.type.map(t => t.toLowerCase()).indexOf(type.toLowerCase()) >= 0);
- 
-//};
-
-
-
-
-
-
-
-
-
-/* Manejo de data */
-
-// esta es una función de ejemplo
-
-
-  //return 'example';
-//};
+export let filtroHAuse = (filtro, house) => {
+  return filtro.filter (filtrohouse => filtrohouse.house.includes(house));
+};
+console.log(filtroHAuse);
--- src/main.js
+++ src/main.js
@@ -1,38 +1,45 @@
  
     import POTTER from './data/potter/potter.js'
     
+
+    
     //btn de la pag principal//
      document.getElementById("btn").addEventListener("click", () => {
      
         pag1.style.display = "none";
         root.style.display = "block";
-        const text = document.getElementById("nom").value;
-        const text1 =document.getElementById("nom1");
-        console.log(text);
-        text1.innerHTML = text.value;
     })
+
     //btn filtrado//
-    let harryF = [];
-    const data = document.getElementById("btn2").addEventListener("click", () =>{
-    //con el for recorre toda la data//
-    for(let i = 0; i < POTTER.length; i++){
-        const btn = POTTER[i];
+    //let harryF = [];
+    document.getElementById("btn2").addEventListener("click", () =>{
+    
+        //con el for recorre toda la data//
+    
+        for(let i = 0; i < POTTER.length; i++){
+        //const btn = POTTER[i];
         console.log(POTTER[i]);
+        contaier.innerHTML += `<div>
+                                    <li> 
+                                    ${POTTER[i].name}
+                                    
+                                     
+                                    </li>
+                                </div>`
         }
         
-    })
-//hacer console.log con todas las caracteristicas//
+    });
+    
+    
 
- //console.log(POTTER)
- //console.log(POTTER[0].name);
- //console.log(POTTER[1].species);
- //console.log(POTTER[2].gender);
- //console.log(POTTER[3].house);
- //console.log(POTTER[4].dateOfBirth);
- //console.log(POTTER[5].yearOfBirth);
- //console.log(POTTER[6].ancestry);
- //console.log(POTTER[7].eyeColour);
- //console.log(POTTER[8].hairColour);
+
+    //filtro por casas//
+    let selectionHouse = POTTER.filter(elemento => (elemento.house === "Gryffindor"));
+    console.log(selectionHouse);
+    
+      
+
+      
 
     
     
