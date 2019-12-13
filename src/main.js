25c050d18107be3fbb413b5b8a8a1891b1664edb
Vaitiare0402 <ihopetimedoesntchangehim@gmail.com>
2019-12-13 10:28:12 -0300

    termino

--- src/main.js
+++ src/main.js
@@ -6,38 +6,39 @@ import{ filtroS } from './data.js';
 const dataFiltadoS= filtroS(POTTER)
 
 import{ filtroH } from './data.js';
-const dataFiltadoH=filtroH(POTTER)
+const dataFiltadoH= filtroH(POTTER)
 
 import{ filtroR } from './data.js';
-const dataFiltadoR=filtroR(POTTER)
+const dataFiltadoR= filtroR(POTTER)
     
 
     
     //btn de la pag principal//
      document.getElementById("btn").addEventListener("click", () => {
-     
         pag1.style.display = "none";
         root.style.display = "block";
-    })
+      
+    });
 
     
     document.getElementById("btn2").addEventListener("click", () =>{
     
-        //con el for recorre toda la data//
+        //con el for recorre el nuevo array que se formo con el filter//
         let selectionHouse = dataFiltrado;
         for (let i=0; i < selectionHouse.length; i++){
             contaier.innerHTML += `<div class= "info">
+            
             <img src=${selectionHouse[i].image} class="fotos"/>
             <li class="description">Name: ${selectionHouse[i].name}
-            <li class="description">Species:${selectionHouse[i].species}
-            <li class="description">Gender:${selectionHouse[i].gender}
-            <li class="description">House:${selectionHouse[i].house}
+            <li class="description">Species: ${selectionHouse[i].species}
+            <li class="description">Gender: ${selectionHouse[i].gender}
+            <li class="description">House: ${selectionHouse[i].house}
             <li class="description">Date of birthday: ${selectionHouse[i].dateOfBirth}
             <li class="description">Year of birthday: ${selectionHouse[i].yearOfBirth}
-            <li class="description">Ancestry:${selectionHouse[i].ancestry}
-            <li class="description">Eye colour:${selectionHouse[i].eyeColour}
-            <li class="description">Patronus:${selectionHouse[i].patronus}
-            <li class="description">Actor:${selectionHouse[i].actor}
+            <li class="description">Ancestry: ${selectionHouse[i].ancestry}
+            <li class="description">Eye colour: ${selectionHouse[i].eyeColour}
+            <li class="description">Patronus: ${selectionHouse[i].patronus}
+            <li class="description">Actor: ${selectionHouse[i].actor}
                                    
            </div>`
 
@@ -50,16 +51,16 @@ const dataFiltadoR=filtroR(POTTER)
         for (let i=0; i < selectionSlytherin.length; i++){
             contaier.innerHTML += `<div class= "info">
             <img src=${selectionSlytherin[i].image} class="fotos"/>
-            <li class="description">Name:${selectionSlytherin[i].name}
-            <li class="description">Species:${selectionSlytherin[i].species}
-            <li class="description">Gender:${selectionSlytherin[i].gender}
+            <li class="description">Name: ${selectionSlytherin[i].name}
+            <li class="description">Species: ${selectionSlytherin[i].species}
+            <li class="description">Gender: ${selectionSlytherin[i].gender}
             <li class="description">House: ${selectionSlytherin[i].house}
-            <li class="description">Date of birthday:${selectionSlytherin[i].dateOfBirth}
-            <li class="description">Year of birthday:${selectionSlytherin[i].yearOfBirth}
-            <li class="description">Ancestry:${selectionSlytherin[i].ancestry}
-            <li class="description">Eye colour:${selectionSlytherin[i].eyeColour}
-            <li class="description">Patronus:${selectionSlytherin[i].patronus}
-            <li class="description">Actor:${selectionSlytherin[i].actor}
+            <li class="description">Date of birthday: ${selectionSlytherin[i].dateOfBirth}
+            <li class="description">Year of birthday: ${selectionSlytherin[i].yearOfBirth}
+            <li class="description">Ancestry: ${selectionSlytherin[i].ancestry}
+            <li class="description">Eye colour: ${selectionSlytherin[i].eyeColour}
+            <li class="description">Patronus: ${selectionSlytherin[i].patronus}
+            <li class="description">Actor: ${selectionSlytherin[i].actor}
 
                         
             </div>`
@@ -72,20 +73,20 @@ const dataFiltadoR=filtroR(POTTER)
         let selectionHufflepuff = dataFiltadoH;
         for (let i=0; i < selectionHufflepuff.length; i++){
             contaier.innerHTML += `<div class= "info">
-            <img src=${selectionHufflepuff[i].image}class="fotos"/>
-            <li class="description">Name:${selectionHufflepuff[i].name}
-            <li class="description">Species:${selectionHufflepuff[i].species}
-            <li class="description">Gender:${selectionHufflepuff[i].gender}
+            <img src=${selectionHufflepuff[i].image} class="fotos"/>
+            <li class="description">Name: ${selectionHufflepuff[i].name}
+            <li class="description">Species: ${selectionHufflepuff[i].species}
+            <li class="description">Gender: ${selectionHufflepuff[i].gender}
             <li class="description">House: ${selectionHufflepuff[i].house}
-            <li class="description">Date of birthday:${selectionHufflepuff[i].dateOfBirth}
-            <li class="description">Year of birthday:${selectionHufflepuff[i].yearOfBirth}
-            <li class="description">Ancestry:${selectionHufflepuff[i].ancestry}
-            <li class="description">Eye colour:${selectionHufflepuff[i].eyeColour}
-            <li class="description">Patronus:${selectionHufflepuff[i].patronus}
-            <li class="description">Actor:${selectionHufflepuff[i].actor}
+            <li class="description">Date of birthday: ${selectionHufflepuff[i].dateOfBirth}
+            <li class="description">Year of birthday: ${selectionHufflepuff[i].yearOfBirth}
+            <li class="description">Ancestry: ${selectionHufflepuff[i].ancestry}
+            <li class="description">Eye colour: ${selectionHufflepuff[i].eyeColour}
+            <li class="description">Patronus: ${selectionHufflepuff[i].patronus}
+            <li class="description">Actor: ${selectionHufflepuff[i].actor}
 
-                                   
-           </div>`
+                        
+            </div>`
         }
 });
 
@@ -94,20 +95,18 @@ const dataFiltadoR=filtroR(POTTER)
         let selectionRavenclaw = dataFiltadoR;
         for (let i=0; i < selectionRavenclaw.length; i++){
             contaier.innerHTML += `<div class= "info">
-            <img src=${selectionRavenclaw[i].image}class="fotos"/>
-            <li class="description">Name:${selectionRavenclaw[i].name}
-            <li class="description">Species:${selectionRavenclaw[i].species}
-            <li class="description">Gender:${selectionRavenclaw[i].gender}
+            <img src=${selectionRavenclaw[i].image} class="fotos"/>
+            <li class="description">Name: ${selectionRavenclaw[i].name}
+            <li class="description">Species: ${selectionRavenclaw[i].species}
+            <li class="description">Gender: ${selectionRavenclaw[i].gender}
             <li class="description">House: ${selectionRavenclaw[i].house}
-            <li class="description">Date of birthday:${selectionRavenclaw[i].dateOfBirth}
-            <li class="description">Year of birthday:${selectionRavenclaw[i].yearOfBirth}
-            <li class="description">Ancestry:${selectionRavenclaw[i].ancestry}
-            <li class="description">Eye colour:${selectionRavenclaw[i].eyeColour}
-            <li class="description">Patronus:${selectionRavenclaw[i].patronus}
-            <li class="description">Actor:${selectionRavenclaw[i].actor}
+            <li class="description">Date of birthday: ${selectionRavenclaw[i].dateOfBirth}
+            <li class="description">Year of birthday: ${selectionRavenclaw[i].yearOfBirth}
+            <li class="description">Ancestry: ${selectionRavenclaw[i].ancestry}
+            <li class="description">Eye colour: ${selectionRavenclaw[i].eyeColour}
+            <li class="description">Patronus: ${selectionRavenclaw[i].patronus}
+            <li class="description">Actor: ${selectionRavenclaw[i].actor}
 
-                            
-            
-       </div>`
-    }
+            </div>`
+       }
 });
